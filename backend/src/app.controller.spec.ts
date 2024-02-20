fbac061bfca0145407c3ee381739abebf1bad3f9
Denys Chepiha <dchepiha@exoft.net>
2024-02-20 23:50:41 +0100

    Test CI/CD

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World! 22');
+    it('should return "CDenys"', () => {
+      expect(appController.getHello()).toBe('CDenys');
     });
   });
 });
